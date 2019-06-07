5524f960e87e57443b12f5051eac8f5ab201ab8a
Unknown <47182802+UnknownOre@users.noreply.github.com>
2019-06-07 15:38:46 +0300

    Update Mobstacker.php

--- src/UnknownOre/MobStacker/Mobstacker.php
+++ src/UnknownOre/MobStacker/Mobstacker.php
@@ -10,52 +10,66 @@ use pocketmine\Player;
 use pocketmine\utils\TextFormat as C;
 use slapper\entities\SlapperEntity;
 
-class Mobstacker{
+<?php
 
+
+namespace UnknownCore\Manager;
+
+use pocketmine\entity\Living;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as C;
+
+class Mobstacker{
     /* @var Living */
     private $entity;
+    
+    CONST STACK = 'stack';
 
+    /**
+     * Mobstacker constructor.
+     * @param Living $entity
+     */
     public function __construct(Living $entity){
         $this->entity = $entity;
     }
-
+    
     /**
      * @return int
      */
     public function getStackAmount(): int{
-        return $this->entity->namedtag->getInt('stack');
+        return $this->entity->namedtag->getInt(self::STACK);
     }
-
+    
     /**
      * @return bool
      */
     public function isStacked(): bool{
-        return $this->entity->namedtag->hasTag('stack');
+        return $this->entity->namedtag->hasTag(self::STACK);
     }
-
     public function Stack(): void{
         if($this->isStacked()){
             $this->updateNameTag();
             return;
         }
         if(($mob = $this->findNearStack()) == null){
-            $nbt = new IntTag('stack',1);
+            $nbt = new IntTag(self::STACK,1);
             $this->entity->namedtag->setTag($nbt);
             $mobstack = $this;
         }else{
             $this->entity->flagForDespawn();
             $mobstack = new Mobstacker($mob);
-            $mob->namedtag->setInt('stack', $mob->namedtag->getInt('stack') + 1);
+            $count = $mob->namedtag->getInt(self::STACK);
+            $mob->namedtag->setInt(self::STACK, ++$count);
         }
         $mobstack->updateNameTag();
     }
-
     public function updateNameTag(): void{
         $nbt = $this->entity->namedtag;
         $this->entity->setNameTagAlwaysVisible(True);
-        $this->entity->setNameTag(C::BLUE.'x'.C::YELLOW.$nbt->getInt("stack").' '.C::YELLOW.$this->entity->getName());
+        $this->entity->setNameTag(C::BLUE.'x'.C::YELLOW.$nbt->getInt(self::STACK).' '.C::YELLOW.$this->entity->getName());
     }
-
     /**
      * @return bool
      */
@@ -63,14 +77,13 @@ class Mobstacker{
         $entity = $this->entity;
         $nbt = $entity->namedtag;
         if(!$this->isStacked() or ($c = $this->getStackAmount()) <= 1) return false;
-        $nbt->setInt('stack',$c - 1);
+        $nbt->setInt(self::STACK,--$c);
         $event = new EntityDeathEvent($entity, $drops = $entity->getDrops());
         $event->call();
         $this->updateNameTag();
         foreach($drops as $drop) $entity->getLevel()->dropItem($entity->getPosition(),$drop);
         return true;
     }
-
     /**
      * @param int $range
      * @return Living|null
